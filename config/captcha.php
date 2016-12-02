c2c0954e80507674846ff3cfe5d5f203a778f206
benjamin <benjaminchen.c@gmail.com>
2016-12-02 10:09:49 +0800

    add config

---
+++ config/captcha.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    're_cap_site' => env('RE_CAP_SITE', '6Lf0pA0UAAAAAJ53efvkxRFF39tvVJMjq8O4cVWQ'),
+    're_cap_secret' => env('RE_CAP_SECRET', '6Lf0pA0UAAAAAE-4c8a4vPRqX2yOYzRG5aklhyUk')
+];
